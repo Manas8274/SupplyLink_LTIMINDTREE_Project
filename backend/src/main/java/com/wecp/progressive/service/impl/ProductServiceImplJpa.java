9892adbf4102d8a4298f120751f638e2f524f552
username <wecp-dummy@gmail.com>
2025-09-04 11:08:12 +0000

    Day 8 Completed

--- backend/src/main/java/com/wecp/progressive/service/impl/ProductServiceImplJpa.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/ProductServiceImplJpa.java
@@ -1,5 +1,55 @@
 package com.wecp.progressive.service.impl;
 
-public class ProductServiceImplJpa  {
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.wecp.progressive.dao.ProductDAO;
+import com.wecp.progressive.entity.Product;
+import com.wecp.progressive.repository.ProductRepository;
+import com.wecp.progressive.service.ProductService;
+
+@Service
+public class ProductServiceImplJpa implements ProductService  {
+
+    private ProductRepository productRepository;
+
+    public ProductServiceImplJpa (ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+   
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Product getProductById(int productId) {
+        return productRepository.findById(productId).get();
+    }
+
+    @Override
+    public int addProduct(Product product) {
+        return productRepository.save(product).getProductId();
+    }
+
+    @Override
+    @Transactional
+    public void updateProduct(Product product) {
+        productRepository.save(product);
+    }
+
+    @Override
+    @Transactional
+    public void deleteProduct(int productId) {
+        productRepository.deleteById(productId);
+        
+    }
+
+    public List<Product> getAllProductByWarehouse(int warehouseId) {
+        return productRepository.findAllByWarehouse_WarehouseId(warehouseId);
+    }
 
 }
