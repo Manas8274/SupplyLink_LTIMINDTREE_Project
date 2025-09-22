df2e5f48ea4c2064a6e334707e63b409aae874f4
username <wecp-dummy@gmail.com>
2025-09-22 19:08:35 +0000

    Day 25 Not Passed

--- frontend/src/app/supplylink/components/warehousesample/warehousesample.component.ts
+++ frontend/src/app/supplylink/components/warehousesample/warehousesample.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Warehouse } from '../../types/Warehouse';
 
 @Component({
   selector: 'app-warehousesample',
@@ -10,3 +11,4 @@ import { Component } from '@angular/core';
 export class WarehouseSampleComponent {
 
 }
+
